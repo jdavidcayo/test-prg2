4acf10255d42e130605d0c5d9a4ef0c8cc5c7078
J David Cayo <jdavidcayo@gmail.com>
2024-03-14 01:51:14 -0300

    Inicio de proyecto.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("IntelliJ IDEA 2023.3.5 (Community Edition)\n" +
+                "Build #IC-233.14808.21, built on March 12, 2024\n" +
+                "Runtime version: 17.0.10+1-b1087.21 amd64\n" +
+                "VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o.\n" +
+                "Linux 5.4.0-58-generic\n" +
+                "GC: G1 Young Generation, G1 Old Generation\n" +
+                "Memory: 2048M\n" +
+                "Cores: 4\n" +
+                "Registry:\n" +
+                "  ide.experimental.ui=true\n" +
+                "Kotlin: 233.14808.21-IJ\n" +
+                "Current Desktop: MATE\n");
+    }
+}
